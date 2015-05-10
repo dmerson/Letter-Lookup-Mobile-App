8b198adf926af3d2bf2a430f7483b24a54acee35
Samuel Merson <sam@digitalarizona.com>
2015-05-10 23:39:00 +0000

    Changes made by Samuel Merson (sam@digitalarizona.com).
    First letter is working Yay!
    All first forms are present.

--- Letter Lookup Hybrid/formView/index.js
+++ Letter Lookup Hybrid/formView/index.js
@@ -7,8 +7,21 @@ app.formView = kendo.observable({
     var formViewModel = kendo.observable({
         fields: {
             letterEntered: '',
+            result:''
         },
-        submit: function() {},
+        submit: function() 
+        {
+           switch (formViewModel.fields.letterEntered.toLowerCase())
+                {
+                    case "a":
+                        alert("1. Yay.");
+                        break;
+                    default:
+                        alert("not implemented yet or not a letter");
+                }
+           alert(formViewModel.fields.letterEntered);
+            
+       	},
         cancel: function() {}
     });
 
