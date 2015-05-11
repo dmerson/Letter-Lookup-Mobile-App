e006f405d9dd3a8c16d0fd1df5de8910ef5aa974
Samuel Merson <sam@digitalarizona.com>
2015-05-11 00:49:27 +0000

    Got word counting working.

--- Letter Lookup Hybrid/formView/index.js
+++ Letter Lookup Hybrid/formView/index.js
@@ -7,85 +7,87 @@ var convert = {
 	GetWordValue: function(valueToCheck) {
 		var finalTally = 0;
 		var lengthOfValue = valueToCheck.length;
-		
+
 		for (lengthOfValue; lengthOfValue > 0; lengthOfValue--) {
-			var addToFinal = convert.GetLetterValue(valueToCheck[lengthOfValue -1]);
-			
-				finalTally = finalTally + addToFinal;
-			
+			var addToFinal = convert.GetLetterValue(valueToCheck[lengthOfValue - 1]);
+			if (addToFinal == NaN) {
+				return NaN;
+			}
+			finalTally = finalTally + addToFinal;
+
 		}
 		return finalTally;
 
 	},
-	GetLetterValue:function(letterToCheck) {
+	GetLetterValue: function(letterToCheck) {
 		switch (letterToCheck) {
-			case "a":
-			case "e":
-			case "i":
-			case "o":
-			case "n":
-			case "r":
-			case "t":
-			case "s":
-			case "l":
-			case "u":
-				{
-					
-					return 1;
-					break;
-				}
-			case "d":
-			case "g":
-				{
-					
-					return 2;
-					break;
-				}
-			case "m":
-			case "p":
-			case "b":
-			case "c":
-			{
-					
-					return 3;
-					break;
-				}
-			case "f":
-			case "h":
-			case "v":
-			case "w":
-			case "y":
-				{
-					
-					return 4;
-					break;
-				}
-			case "k":
-				{
-					
-					return 5;
-					break;
-				}
-			case "j":
-			case "x":
-				{
-					
-					return 8;
-					break;
-				}
-			case "q":
-			case "z":
-				{
-					
-					return 10;
-					break;
-				}
-			default:
-				{
-					
-					return NaN;
-					break;
-				}
+		case "a":
+		case "e":
+		case "i":
+		case "o":
+		case "n":
+		case "r":
+		case "t":
+		case "s":
+		case "l":
+		case "u":
+		{
+
+			return 1;
+			break;
+		}
+		case "d":
+		case "g":
+		{
+
+			return 2;
+			break;
+		}
+		case "m":
+		case "p":
+		case "b":
+		case "c":
+		{
+
+			return 3;
+			break;
+		}
+		case "f":
+		case "h":
+		case "v":
+		case "w":
+		case "y":
+		{
+
+			return 4;
+			break;
+		}
+		case "k":
+		{
+
+			return 5;
+			break;
+		}
+		case "j":
+		case "x":
+		{
+
+			return 8;
+			break;
+		}
+		case "q":
+		case "z":
+		{
+
+			return 10;
+			break;
+		}
+		default:
+		{
+
+			return NaN;
+			break;
+		}
 		}
 	}
 };
@@ -97,8 +99,14 @@ var convert = {
 		},
 		submit: function() {
 
-		
-			formViewModel.fields.set("result", convert.GetWordValue(formViewModel.fields.letterEntered.toLowerCase()));
+			var result = convert.GetWordValue(formViewModel.fields.letterEntered.toLowerCase());
+			if (isNaN(result)) {
+				formViewModel.fields.set("result", "Not valid word");
+				return;
+			}
+					formViewModel.fields.set("result", result);
+			
+			
 
 
 		},
