3046951aff4e2b41f346af6476638b803f67ab5e
Samuel Merson <sam@digitalarizona.com>
2015-05-11 00:13:38 +0000

    Changes made by Samuel Merson (sam@digitalarizona.com).
    All letters made to work using Scrabble point values.

--- Letter Lookup Hybrid/formView/index.js
+++ Letter Lookup Hybrid/formView/index.js
@@ -1,32 +1,80 @@
 'use strict';
 
 app.formView = kendo.observable({
-    onShow: function() {}
+    onShow: function () {}
 });
-(function(parent) {
+(function (parent) {
     var formViewModel = kendo.observable({
         fields: {
             letterEntered: '',
-            result:''
+            result: ''
         },
-        submit: function() {
-	        debugger;
-           switch (formViewModel.fields.letterEntered.toLowerCase())
-                {
-           	case "a":
-	           {
-		           formViewModel.fields.set("result", "1");
-		           break;
-	           }
-	           default:
+        submit: function () {
+
+            switch (formViewModel.fields.letterEntered.toLowerCase()) {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "n":
+                case "r":
+                case "t":
+                case "s":
+                case "l":
+                case "u":
                     {
-	                    break;
+                        formViewModel.fields.set("result", "1");
+                        break;
                     }
-                }
-          
-            
-       	},
-        cancel: function() {}
+                case "d":
+                case "g":
+                    {
+                        formViewModel.fields.set("result", "2");
+                        break;
+                    }
+                case "d":
+                case "g":
+                case "b":
+                case "c":
+                    {
+                        formViewModel.fields.set("result", "3");
+                        break;
+                    }
+                case "f":
+                case "h":
+                case "v":
+                case "w":
+                case "y":
+					{
+                        formViewModel.fields.set("result","4");
+                        break;
+                    }
+                case "k":
+                    {
+                        formViewModel.fields.set("result","5");
+                        break;
+                    }
+                case "j":
+                case "x":
+                    {
+                        formViewModel.fields.set("result", "8");
+                        break;
+                    }
+                case "q":
+                case "z":
+                    {
+                        formViewModel.fields.set("result", "10");
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+
+
+        },
+        cancel: function () {}
     });
 
     parent.set('formViewModel', formViewModel);
