d50754318bf37dc7a2e8be2c78fac7453402505a
Samuel Merson <sam@digitalarizona.com>
2015-05-11 00:30:35 +0000

    Changes made by Samuel Merson (sam@digitalarizona.com). Refactored switch statement to return value.

--- Letter Lookup Hybrid/formView/index.js
+++ Letter Lookup Hybrid/formView/index.js
@@ -1,81 +1,103 @@
-'use strict';
+"use strict";
 
 app.formView = kendo.observable({
-    onShow: function () {}
+	onShow: function() {}
 });
-(function (parent) {
-    var formViewModel = kendo.observable({
-        fields: {
-            letterEntered: '',
-            result: ''
-        },
-        submit: function () {
+var convertModel = {
+	GetValue: function(valueToCheck) {
+		debugger;
+		var lengthOfValue = valueToCheck.length;
+		for (lengthOfValue; lengthOfValue > 0; lengthOfValue--) {
+			console.log(lengthOfValue);
+		}
 
-            switch (formViewModel.fields.letterEntered.toLowerCase()) {
-                case "a":
-                case "e":
-                case "i":
-                case "o":
-                case "n":
-                case "r":
-                case "t":
-                case "s":
-                case "l":
-                case "u":
-                    {
-                        formViewModel.fields.set("result", "1");
-                        break;
-                    }
-                case "d":
-                case "g":
-                    {
-                        formViewModel.fields.set("result", "2");
-                        break;
-                    }
-                case "d":
-                case "g":
-                case "b":
-                case "c":
-                    {
-                        formViewModel.fields.set("result", "3");
-                        break;
-                    }
-                case "f":
-                case "h":
-                case "v":
-                case "w":
-                case "y":
-					{
-                        formViewModel.fields.set("result","4");
-                        break;
-                    }
-                case "k":
-                    {
-                        formViewModel.fields.set("result","5");
-                        break;
-                    }
-                case "j":
-                case "x":
-                    {
-                        formViewModel.fields.set("result", "8");
-                        break;
-                    }
-                case "q":
-                case "z":
-                    {
-                        formViewModel.fields.set("result", "10");
-                        break;
-                    }
-                default:
-                    {
-                        break;
-                    }
-            }
+	},
+	GetLetterValue:function(letterToCheck) {
+		switch (letterToCheck) {
+			case "a":
+			case "e":
+			case "i":
+			case "o":
+			case "n":
+			case "r":
+			case "t":
+			case "s":
+			case "l":
+			case "u":
+				{
+					
+					return 1;
+					break;
+				}
+			case "d":
+			case "g":
+				{
+					
+					return 2;
+					break;
+				}
+			case "d":
+			case "g":
+			case "b":
+			case "c":
+				{
+					
+					return 3;
+					break;
+				}
+			case "f":
+			case "h":
+			case "v":
+			case "w":
+			case "y":
+				{
+					
+					return 4;
+					break;
+				}
+			case "k":
+				{
+					
+					return 5;
+					break;
+				}
+			case "j":
+			case "x":
+				{
+					
+					return 8;
+					break;
+				}
+			case "q":
+			case "z":
+				{
+					
+					return 10;
+					break;
+				}
+			default:
+				{
+					
+					return NaN;
+					break;
+				}
+		}
+	}
+};
+(function(parent) {
+	var formViewModel = kendo.observable({
+		fields: {
+			letterEntered: "",
+			result: ""
+		},
+		submit: function() {
+
+			formViewModel.fields.set("result", convertModel.GetLetterValue(formViewModel.fields.letterEntered.toLowerCase()));
 
 
-        },
-        cancel: function () {}
-    });
+		},
+		cancel: function() {}
+	});
 
-    parent.set('formViewModel', formViewModel);
+	parent.set("formViewModel", formViewModel);
 })(app.formView);
